4fc79e1bfd58c686817469279782a42740524c9e
SebLay <sebastien.lannoy98@gmail.com>
2024-06-12 13:57:04 +0200

    add testing for winngin condition

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
